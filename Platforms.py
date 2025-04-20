8431c5803854ea0fdfe16b4a0fe9903acc7ee49f
Weeqeen <Ilya.Sbrodov@urfu.me>
2025-04-20 19:59:30 +0500

    Update Platforms.py

--- Platforms.py
+++ Platforms.py
@@ -4,7 +4,7 @@ from load_image import load_image
 
 pygame.init()
 
-PLATFORM_COLOR = (50, 150, 50)
+platform_ = pygame.image.load("Sprites\\tile.png")
 GREEN_GROUND = (0, 200, 0)
 
 
@@ -13,7 +13,7 @@ class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
-        self.image.fill(PLATFORM_COLOR)
+        self.image.blit(platform_, (0,0))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -31,7 +31,7 @@ class Platform_Ground(Platform):
 class FinishSprite(pygame.sprite.Sprite):
     def __init__(self, x, y, image_path):
         pygame.sprite.Sprite.__init__(self)
-        self.image = load_image(image_path, 0.1)
+        self.image = load_image(image_path, 0.2)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
