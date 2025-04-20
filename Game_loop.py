8d5a2ee49d3d9c20f9e4a0718df8421d613f9632
Weeqeen <Ilya.Sbrodov@urfu.me>
2025-04-20 19:59:11 +0500

    Update Game_loop.py

--- Game_loop.py
+++ Game_loop.py
@@ -1,22 +1,59 @@
 import pygame
+from Small_func import quit_game
 from Player import Player
+from numpy.random.mtrand import randint
 from Platforms import Platform, Platform_Ground, FinishSprite
+from Draw_t_and_b import draw_text, draw_button
 
 
 pygame.init()
-SKY = (135, 206, 235)
 WIDTH, HEIGHT = 1024, 650
+bg = pygame.image.load("Sprites\\layer-1.png")
+bg = pygame.transform.scale(bg, (WIDTH, HEIGHT+50 ))
+font = pygame.font.SysFont(None, 48)
+FONT_COLOR = (255, 255, 255)
 level = 1
 screen = pygame.display.set_mode((1024, 650))
 
 
+def random_level():
+    level = randint(1,6)
+    return game_loop(level)
+
+
+def end_menu():
+    """Меню завершения игры после 5 уровня"""
+    clock = pygame.time.Clock()  # Инициализируем clock внутри функции
+    running = True
+    while running:
+        screen.blit(bg, (0, 0))
+
+        draw_text('Поздравляем, вы прошли игру!', font, FONT_COLOR, screen, WIDTH // 2, HEIGHT // 4)
+
+        # Кнопка для начала игры заново
+        draw_button('Начать заново', font, screen, WIDTH // 2 - 130, HEIGHT // 2 - 50, 260, 60,
+                    action=lambda: game_loop(level=1))
+
+        # Кнопка для выхода
+        draw_button('Выход', font, screen, WIDTH // 2 - 130, HEIGHT // 2 + 50, 260, 60, quit_game)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_game()
+
+        pygame.display.flip()
+        clock.tick(60)  # Обновляем экран 60 раз в секунду
+
+
+
 def game_loop(level):
     all_sprites = pygame.sprite.Group()
+    font_small = pygame.font.SysFont(None, 30)
     player = Player()
     all_sprites.add(player)
 
     platforms = pygame.sprite.Group()
-    platform = Platform_Ground(0, HEIGHT + 40, WIDTH + 100 , 100, "Sprites\\Grass.png")
+    platform = Platform_Ground(0, HEIGHT + 40, WIDTH + 100, 100, "Sprites\\layer-2.png")
     all_sprites.add(platform)
     platforms.add(platform)
 
@@ -36,10 +73,10 @@ def game_loop(level):
             (830, 390, 150, 20),
             (150, 220, 150, 20),
             (300, 420, 150, 20),
-            (600, 320, 150, 20),
-            ],
+            (600, 320, 150, 20)
+        ],
         3: [
-            (50, 500, 200, 20),
+            (50, 600, 200, 20),
             (300, 400, 200, 20),
             (550, 300, 200, 20),
             (200, 200, 200, 20),
@@ -62,11 +99,11 @@ def game_loop(level):
     }
 
     finish_positions = {
-        1: (WIDTH - 100, HEIGHT - 600),
-        2: (WIDTH - 150, HEIGHT - 400),
-        3: (WIDTH - 120, HEIGHT - 300),
-        4: (WIDTH - 100, HEIGHT - 200),
-        5: (WIDTH - 80, HEIGHT - 100)
+        1: (WIDTH - 100, HEIGHT - 0),
+        2: (WIDTH - 150, HEIGHT - 0),
+        3: (WIDTH - 120, HEIGHT - 0),
+        4: (WIDTH - 100, HEIGHT - 0),
+        5: (WIDTH - 80, HEIGHT - 0)
     }
 
     for x, y, width, height in platform_positions[level]:
@@ -74,7 +111,7 @@ def game_loop(level):
         all_sprites.add(platform)
         platforms.add(platform)
 
-    finish_image_path = "C:\\Users\\student\\PycharmProjects\\PythonProject5\\.venv\\Sprites\\finish_image.jpg"
+    finish_image_path = "C:\\Users\\Илья\\PycharmProjects\\PyGame\\.venv\\Sprites\\finish_image.jpg"
     finish_x, finish_y = finish_positions[level]
     finish_sprite = FinishSprite(finish_x, finish_y, finish_image_path)
     all_sprites.add(finish_sprite)
@@ -88,7 +125,7 @@ def game_loop(level):
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                quit_game()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     player.jump()
@@ -97,35 +134,27 @@ def game_loop(level):
                 if event.key == pygame.K_RIGHT:
                     player.x_speed = 5
                 if event.key == pygame.K_DOWN:
-                    if player.rect.bottom < HEIGHT - 50:
-                        player.rect.y += 5
+                    player.drop_down()
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     player.x_speed = 0
 
-        all_sprites.update()
+        all_sprites.update(platforms)
 
-        collisions = pygame.sprite.spritecollide(player, platforms, False)
-        if collisions:
-            player.rect.bottom = collisions[0].rect.top
-            player.y_speed = 0
-            player.is_jumping = False
+
 
         if pygame.sprite.collide_rect(player, finish_sprite):
-            if level >= 5:
-                draw_text('       Победа!', font, screen, WIDTH // 2 - 100, HEIGHT // 2 - 50)
-                draw_button('Начать заново?', font, screen, WIDTH // 2 - 100, HEIGHT // 2 + 20, 270, 50,
-                            lambda: reset_game())
-                draw_button('Выйти из игры', font, screen, WIDTH // 2 - 100, HEIGHT // 2 + 80, 270, 50, pygame.quit)
-                pygame.display.flip()
-                waiting_for_click = True
-                continue
+            if level == 5:
+                running = False
+                end_menu()
             else:
                 level += 1
                 game_loop(level)
 
         if not waiting_for_click:
-            screen.fill(SKY)
+            screen.blit(bg, (0, 0))
+            draw_text(f"Уровень {level}", font_small, FONT_COLOR, screen, WIDTH - 50, 10)
             all_sprites.draw(screen)
             pygame.display.flip()
 
